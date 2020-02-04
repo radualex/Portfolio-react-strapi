8b7b58dcd19161f9168898977f5bbd7fc22c95d1
Radu-Alexandru Stoica <radu.stoica1994@gmail.com>
2020-02-04 20:31:15 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Projects from './Projects.js'; 
+import Articles from './Articles.js'; 
+import About from './About.js'; 
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+  
+        <Route exact path="/" component={Projects} />
+        <Route path="/articles" component={Articles} />
+        <Route path="/about" component={About} />
+  
+        <div className="navigation">
+          <img src={logo} className="logo-image" alt="Logo Image" />
+          <div className="navigation-sub">
+                                          
+            <Link to="/" className="item">Projects</Link>
+            <Link to="/articles" className="item">Articles</Link>
+            <Link to="/about" className="item">About</Link>
+  
+          </div>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;

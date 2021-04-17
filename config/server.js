d4d62614cb72a8559a700857f6f78c50034dc45e
Radu-Alexandru Stoica <radu.stoica1994@gmail.com>
2021-04-18 00:18:40 +0200

    reset project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '26de86b67e0ec1848f94db1063e6a870'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a58f8750-19a6-4e6f-96a8-f9595da1e6ee'
+};
